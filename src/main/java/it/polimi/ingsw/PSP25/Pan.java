3b1657b3fb0208191252aa3499593be2fdd38e75
claudioEma <claudioemanuele9898@gmail.com>
2020-03-27 23:53:18 +0100

    Implemented some god powers

--- src/main/java/it/polimi/ingsw/PSP25/Pan.java
+++ src/main/java/it/polimi/ingsw/PSP25/Pan.java
@@ -1,4 +1,12 @@
 package it.polimi.ingsw.PSP25;
 
 public class Pan extends GodPower {
+
+    @Override
+    protected boolean verifyWin(Worker worker) {
+        if (super.verifyWin(worker) || worker.getHeightBeforeMove() - worker.getSpace().getTowerHeight() >= 2)
+            return true;
+        else
+            return false;
+    }
 }
