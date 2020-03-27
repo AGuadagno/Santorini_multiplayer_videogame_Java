721f7f7dbcd04f6d2758174471d71fbd1c809b81
claudioEma <claudioemanuele9898@gmail.com>
2020-03-27 23:53:18 +0100

    Implemented some god powers

--- src/main/java/it/polimi/ingsw/PSP25/Atlas.java
+++ src/main/java/it/polimi/ingsw/PSP25/Atlas.java
@@ -1,4 +1,18 @@
 package it.polimi.ingsw.PSP25;
 
+import java.util.Scanner;
+
 public class Atlas extends GodPower {
+
+    @Override
+    protected void buildBlock(Space space) {
+        if (space.getTowerHeight() < 3) {
+            System.out.println("Do you want to build a dome or a block");
+            Scanner scanner = new Scanner(System.in);
+            String answer = scanner.next();
+            if (answer.equals("block"))
+                space.increaseTowerHeight();
+        }
+        space.addDome();
+    }
 }
--- src/main/java/it/polimi/ingsw/PSP25/Minotaur.java
+++ src/main/java/it/polimi/ingsw/PSP25/Minotaur.java
@@ -1,4 +1,47 @@
 package it.polimi.ingsw.PSP25;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Minotaur extends GodPower {
+
+    @Override
+    protected List<Space> getValidMovementSpaces(Worker worker) {
+        ArrayList<Space> validMovementSpaces = new ArrayList<Space>();
+
+        for (Space space : worker.getSpace().getAdjacentSpaces()) {
+
+            Space spaceSameDir = spaceSameDir(space, worker.getSpace());
+            if ((space.getWorker() == null || (spaceSameDir != null && spaceSameDir.getWorker() == null && !spaceSameDir.hasDome()))
+                    && space.getTowerHeight() - worker.getSpace().getTowerHeight() <= 1 && !space.hasDome()) {
+                validMovementSpaces.add(space);
+            }
+        }
+        return validMovementSpaces;
+    }
+
+    @Override
+    protected void moveWorker(Worker worker, Space space) {
+        if (space.hasWorker()) {
+            Space spaceSameDir = spaceSameDir(space, worker.getSpace());
+            space.getWorker().moveTo(spaceSameDir);
+        }
+        worker.moveTo(space);
+    }
+
+    private Space spaceSameDir(Space space1, Space space2) {
+
+        int dirX = space1.getX() - space2.getX();
+        int dirY = space1.getY() - space2.getY();
+        List<Space> adiacentSpaces = space1.getAdjacentSpaces();
+        Space spaceSameDir = null;
+        for (Space space : adiacentSpaces) {
+            if (space.getX() == space1.getX() + dirX && space.getY() == space1.getY() + dirY) {
+                spaceSameDir = space2;
+                break;
+            }
+        }
+        return spaceSameDir;
+    }
+
 }
